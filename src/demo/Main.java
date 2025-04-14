3ebc8731a30e27e8d86df7d2e189e7356fe3ecda
Danail Zahariev <d.zahariev@icloud.com>
2025-04-14 20:44:57 +0300

    added new for

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -6,5 +6,9 @@ public class Main {
         for (int i = 1; i <= 10 ; i++) {
             System.out.println(i);
         }
+
+        for (int i = 1; i < 100; i++) {
+            System.out.println(i);
+        }
     }
 }
